9e8e23140fcb1fd0e9bd20b3c236d30a6c3e2ed9
Liz <stokesec@hotmail.com>
2021-11-18 15:07:47 +1300

    World 1 | 2 Built

---
+++ Assets/Liz/Scripts/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class SceneChange : MonoBehaviour
+
+{
+    int i = 0;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(i+1);
+    }
+
+}

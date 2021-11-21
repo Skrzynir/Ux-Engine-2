50261ba8fd16a604144f418ae812355d9551b6bd
Liz <stokesec@hotmail.com>
2021-11-22 12:45:46 +1300

    Interact key change

--- Assets/Liz/Scripts/MirrorMovement.cs
+++ Assets/Liz/Scripts/MirrorMovement.cs
@@ -6,13 +6,8 @@ public class MirrorMovement : MonoBehaviour
 {
     public Transform OriginalVersion;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
+    
     void Update()
     {
         transform.position = new Vector3(OriginalVersion.position.x, transform.position.y, 0);

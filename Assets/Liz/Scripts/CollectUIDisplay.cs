584cc0132f5105ecbab4fb2b71a8890f8838fcaa
Liz <stokesec@hotmail.com>
2021-11-22 15:23:56 +1300

    UI changes | Prompts

---
+++ Assets/Liz/Scripts/CollectUIDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectUIDisplay : MonoBehaviour
+{
+    public Image OrbtoCollect;
+
+
+
+    void Start()
+    {
+        OrbtoCollect.enabled = true;
+        StartCoroutine(ActiveImage());
+    }
+
+   IEnumerator ActiveImage()
+    {
+        yield return new WaitForSecondsRealtime(20.0f);
+        OrbtoCollect.enabled = false;
+    }
+
+}

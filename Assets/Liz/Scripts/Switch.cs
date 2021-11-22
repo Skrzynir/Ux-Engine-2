f268dd2e45b15e4ea6f31485ad941f8ea8bd6069
Irena <skrzynir@gmail.com>
2021-11-22 15:16:31 +1300

    Particle effects start

--- Assets/Liz/Scripts/Switch.cs
+++ Assets/Liz/Scripts/Switch.cs
@@ -7,12 +7,21 @@ public class Switch : MonoBehaviour
     public bool SwitchToPress = false;
     public bool SwitchActive = false;
 
-   
+    SpriteRenderer SR;
+    public Sprite GreySwitch;
+
+    private void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && SwitchToPress == true)
         {
             SwitchActive = true;
+            SR.sprite = GreySwitch;
+            SR.flipY = true;
         }
     }
 
@@ -21,6 +30,7 @@ public class Switch : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {            
             SwitchToPress = true;
+
         }
     }
 }

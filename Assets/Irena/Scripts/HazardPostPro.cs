4db6b04114da2a7d76e2873f403a684bd9db80a2
Irena <skrzynir@gmail.com>
2021-11-23 15:06:53 +1300

    Portal Particles

---
+++ Assets/Irena/Scripts/HazardPostPro.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardPostPro : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject PostPro;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == " Player")
+        {
+            StartCoroutine(Death);
+        }
+    }
+
+    IEnumerator Death()
+    {
+
+    }
+}
